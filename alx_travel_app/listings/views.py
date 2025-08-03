1d1808066781ed98092b486a8db4f56e28099472
Elkan <Elkan@GALAXY-BOOK>
2025-08-03 09:58:22 -0900

    added views

---
+++ alx_travel_app/listings/views.py
@@ -0,0 +1,17 @@
+# listings/views.py
+from rest_framework import viewsets
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+
+class ListingViewSet(viewsets.ModelViewSet):
+    queryset = Listing.objects.all().order_by('-created_at')
+    serializer_class = ListingSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all().order_by('-created_at')
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
